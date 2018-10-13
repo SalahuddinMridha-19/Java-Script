689f23aa671c527b0c342b20bce6687fcbf0fc3a
Md. Sarif <mdshhossain1994@gmail.com>
2018-10-13 18:44:17 +0600

    java

---
+++ java script/java1.js
@@ -0,0 +1,14 @@
+
+
+document.write("Hello World"+ "<br/>");
+var x=10;
+var y=15;
+result = x+y;
+document.write("result= "+result+"<br/>");
+document.write("<br/>")
+var str=" Hello world"+"<br/>";
+
+document.write(str);
+var muti =x * y;
+console.log(multi);
+
